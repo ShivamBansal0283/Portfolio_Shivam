10af331ba84e78acb75c001ae066165cd7a7df6a
Shivam Bansal <144053324+ShivamBansal0283@users.noreply.github.com>
2025-09-08 23:33:20 +0530

    Update awards.ts

--- src/content/awards.ts
+++ src/content/awards.ts
@@ -1,7 +1,9 @@
 // src/content/awards.ts
 const awards: string[] = [
   // fill from resume if you want
-  "Penultimate round in Adobe Hackathon ’24 & ’25",
+  "Finalist - Collins Aerospace AeroHack’25 (28-06-2025)",
+  "Penultimate round in Adobe Hackathon ’25",
+  "Finalist - Adobe Hackathon’24 (12-08-2024)",
   "Winner – TESSERACT, TechTatva’23 (MIT Manipal)",
   "Winner – Trail & Tail, Revel’24 (MIT Manipal)"
 ];
